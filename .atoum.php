c87eec53979f6e53f61613c591a64050463312cb
Cédric Vanet <dev@cedvan.com>
2018-07-16 17:57:55 +0200

    Add badges

--- .atoum.php
+++ .atoum.php
@@ -2,5 +2,13 @@
 
 $runner
     ->addTestsFromDirectory(__DIR__ . '/tests/units/src')
-    ->addTestsFromDirectory(__DIR__ . '/tests/integration/src')
+    ->disallowUsageOfUndefinedMethodInMock()
 ;
+
+if (getenv('TRAVIS') !== false) {
+    $script->addDefaultReport();
+    $cloverWriter = new atoum\writers\file(__DIR__.'/clover.xml');
+    $cloverReport = new atoum\reports\asynchronous\clover();
+    $cloverReport->addWriter($cloverWriter);
+    $runner->addReport($cloverReport);
+}
